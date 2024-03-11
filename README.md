0a2729345aba0cc61a423664b277f2e19691b951
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2024-03-11 14:37:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Dot Dash: Morse Code Translator
+
+Dot Dash is a powerful and user-friendly application that translates Morse code.
+It’s built with Rust, a language renowned for its performance and safety. The
+application is designed to be intuitive and easy to use, making Morse code
+translation accessible to everyone, from hobbyists to professionals.
+
+The application works by accepting Morse code as input and translating it into
+readable text. This can be incredibly useful in a variety of scenarios, such as
+deciphering Morse code messages or learning Morse code for amateur radio usage.
+
+Moreover, Dot Dash is not just a local application. It can also be compiled to
+WebAssembly (WASM) and published as a web page, making it accessible from any
+device with a web browser. This feature broadens the reach of the application,
+allowing users to access it anytime, anywhere.
+
+In addition, Dot Dash is designed with a focus on performance. It leverages the
+power of Rust to ensure that translations are done quickly and efficiently. This
+makes it a reliable tool for anyone who needs to translate Morse code.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Testing Locally](#testing-locally)
+- [Web Locally](#web-locally)
+- [Web Deploy](#web-deploy)
+
+## Getting Started
+
+Ensure you are using the latest version of stable Rust. Update your version by
+running:
+
+```bash
+rustup update
+```
+
+## Testing Locally
+
+To test the application locally, run:
+
+```bash
+cargo run --release
+```
+
+### Linux
+
+If you're on Linux, you need to install some packages first:
+
+```bash
+sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev
+```
+
+### Fedora Rawhide
+
+If you're on Fedora Rawhide, you need to install some packages first:
+
+```bash
+dnf install clang clang-devel clang-tools-extra libxkbcommon-devel pkg-config openssl-devel libxcb-devel gtk3-devel atk fontconfig-devel
+```
+
+## Web Locally
+
+You can compile your app to [WASM](https://en.wikipedia.org/wiki/WebAssembly)
+and publish it as a web page. We use [Trunk](https://trunkrs.dev/) to build for
+the web target.
+
+1. Install the required target with `rustup target add wasm32-unknown-unknown`.
+2. Install Trunk with `cargo install --locked trunk`.
+3. Run `trunk serve` to build and serve on `http://127.0.0.1:8080`. Trunk will
+   rebuild automatically if you edit the project.
+4. Open `http://127.0.0.1:8080/index.html#dev` in a browser.
+
+> Note: The `assets/sw.js` script will try to cache our app, and loads the
+> cached version when it cannot connect to the server allowing your app to work
+> offline (like PWA). Appending `#dev` to `index.html` will skip this caching,
+> allowing us to load the latest builds during development.
+
+## Web Deploy
+
+1. Run `trunk build --release` to generate a `dist` directory as a "static html"
+   website.
+2. Upload the `dist` directory to any of the numerous free hosting websites
+   including
+   [GitHub Pages](https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site).
+3. We already provide a workflow that auto-deploys our app to GitHub pages if
+   you enable it.
+
+> To enable Github Pages, you need to go to Repository -> Settings -> Pages ->
+> Source -> set to `gh-pages` branch and `/` (root). If `gh-pages` is not
+> available in `Source`, just create and push a branch called `gh-pages` and it
+> should be available.
---
+++ src/app.rs
@@ -0,0 +1,105 @@
+use egui::ScrollArea;
+
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct App {
+    message: String,
+    morse_code: String,
+}
+
+
+impl App {
+    
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+
+    fn message_ui(&mut self, ui: &mut egui::Ui) -> egui::Response {
+        let Self {
+            message, ..
+        } = self;
+
+        let _heading_response = {
+            let text = egui::RichText::new("Message").heading();
+            ui.add(egui::Label::new(text))
+        };
+
+        let text_edit_response = 
+            ui.add(egui::TextEdit::multiline(message).min_size(ui.available_size()));
+
+        text_edit_response
+    }
+
+    fn morse_code_ui(&mut self, ui: &mut egui::Ui) -> egui::Response {
+        let Self {
+            morse_code, ..
+        } = self;
+
+        let _heading_response = {
+            let text = egui::RichText::new("Morse Code").heading();
+            ui.add(egui::Label::new(text))
+        };
+        let text_edit_response = 
+            ui.add(egui::TextEdit::multiline(morse_code).min_size(ui.available_size()));
+
+        text_edit_response
+    }
+}
+
+impl eframe::App for App {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                // NOTE: no File->Quit on web pages!
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                ui.menu_button("View", |ui| {
+                    egui::widgets::global_dark_light_mode_buttons(ui);
+                })
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.columns(2, |columns| {
+                ScrollArea::vertical().id_source("message").show(&mut columns[0], |ui| {
+                    if self.message_ui(ui).changed() {
+                        self.morse_code = algorithms::ciphers::morse_code::encrypt(&self.message);
+                    }
+                });
+
+                ScrollArea::vertical().id_source("morse_code").show(&mut columns[1], |ui| {
+                    if self.morse_code_ui(ui).changed() {
+                        self.message = algorithms::ciphers::morse_code::decrypt(&self.morse_code);
+                    };
+                });
+            });
+
+        });
+    }
+}
