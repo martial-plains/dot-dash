27838aa6aa511424475e57d36f443ee0646f3684
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-11-06 12:56:01 -0400

    chore update to 0.6.0-alpha.4

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,46 @@
 use dioxus::prelude::*;
-use dioxus_logger::tracing::Level;
+use dioxus_logger::tracing::{info, Level};
 use routes::{home::Home, page_not_found::PageNotFound};
 
 pub mod platforms;
 pub mod routes;
 
-const STYLE: &str = asset!("./public/styles/tailwind/tailwind.css");
+const STYLE: Asset = asset!("./public/styles/tailwind/tailwind.css");
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
     #[route("/")]
     Home {},
-    // PageNotFound is a catch all route that will match any route and placing the matched segments in the route field
-    #[route("/:..route")]
-    PageNotFound { route: Vec<String> },
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    use dioxus::desktop::{LogicalSize, WindowBuilder};
+    #[cfg(feature = "web")]
+    {
+        dioxus_logger::init(Level::INFO).expect("failed to init logger");
 
-    // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+        launch(app);
+    }
 
-    let cfg = dioxus::desktop::Config::new().with_window(
-        WindowBuilder::new()
-            .with_title("Dot Dash")
-            .with_always_on_top(false)
-            .with_min_inner_size(LogicalSize::new(400, 600)),
-    );
-    LaunchBuilder::desktop().with_cfg(cfg).launch(app);
-}
+    #[cfg(feature = "desktop")]
+    {
+        use dioxus::desktop::{LogicalSize, WindowBuilder};
 
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+        // Init logger
+        dioxus_logger::init(Level::INFO).expect("failed to init logger");
 
-    launch(app);
+        let cfg = dioxus::desktop::Config::new().with_window(
+            WindowBuilder::new()
+                .with_title("Dot Dash")
+                .with_always_on_top(false)
+                .with_min_inner_size(LogicalSize::new(400, 600)),
+        );
+        LaunchBuilder::desktop().with_cfg(cfg).launch(app);
+    }
 }
 
 fn app() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: STYLE }
-        div { class: "bg-transparent dark:bg-slate-800 min-h-screen w-screen", Router::<Route> {} }
+        main { class: "bg-transparent dark:bg-slate-800 min-h-screen w-screen", Router::<Route> {} }
     }
 }
